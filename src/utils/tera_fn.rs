28c55ff0e40257dc497ef0d7b4edfa730dc0be7c
pfwang <pfwang@yoozoo.com>
2024-01-15 22:55:19 +0800

    添加tera自定义filter 支持参数

--- src/utils/tera_fn.rs
+++ src/utils/tera_fn.rs
@@ -4,17 +4,20 @@ use std::time::SystemTime;
 use tera::Value;
 use tera::{to_value, try_get_value, Result};
 
-pub fn date_format(value: &Value, _: &HashMap<String, Value>) -> Result<Value> {
+pub fn date_format(value: &Value, args: &HashMap<String, Value>) -> Result<Value> {
     let s = try_get_value!("date_format", "value", SystemTime, value);
 
     // 将 SystemTime 转换为 UTC DateTime
     let datetime: DateTime<Utc> = s.into();
 
     // 设置格式化字符串
-    let format_string = "%Y-%m-%d %H:%M:%S";
+    let format_string = match args.get("format") {
+        Some(format) => try_get_value!("date_format", "format", String, format),
+        None => String::from("%Y-%m-%d %H:%M:%S"),
+    };
 
     // 格式化日期时间
-    let formatted_time = datetime.format(format_string).to_string();
+    let formatted_time = datetime.format(format_string.as_str()).to_string();
 
     Ok(to_value(formatted_time)?)
 }
